e31514901dae2b029fbeb3f84b137676d2aee088
Venkata Lokesh ANNE_MeritisFinance <venkata-lokesh.anne@meritis.fr>
2026-06-18 16:52:39 +0200

    Update EKS node group instance type and ignore .env

--- EKS-TF/eks-node-group.tf
+++ EKS-TF/eks-node-group.tf
@@ -11,7 +11,7 @@ resource "aws_eks_node_group" "eks-node-group" {
     min_size     = 1
   }
 
-  instance_types = ["t3a.medium"]
+  instance_types = ["t2.small"]
   disk_size      = 20
 
   depends_on = [
